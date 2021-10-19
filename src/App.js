ea5e334fde83c8643b3d8f22f0ace7e1e1e0ccc6
Łukasz Klekner <lukaszklekner@gmail.com>
2021-10-19 16:34:07 +0200

    implements Home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
       <div className='content'>
-        <h1>App Component</h1>
+        <Home />
       </div>
     </div>
   )
