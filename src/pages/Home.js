f76a941f27b2af7028c78a461a8341f2ce202aef
Łukasz Klekner <lukaszklekner@gmail.com>
2021-10-20 18:32:06 +0200

    adds two states and uses them

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
+import { useState } from 'react'
+
 const Home = () => {
-  const handleClick = (name, e) => {
-    console.log(`Hello ${name}`, e.target)
+  const [name, setName] = useState('Mario')
+  const [age, setAge] = useState(25)
+
+  const handleClick = () => {
+    setName('Lukasz')
+    setAge(27)
   }
   return (
     <div className='home'>
       <h2>Homepage</h2>
+      <p>
+        {name} is {age} years old
+      </p>
       <button onClick={(e) => handleClick('Lukas', e)}>Click me!</button>
     </div>
   )
