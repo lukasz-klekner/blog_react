eb4fc3b9f19acb655abe95b19916c6018f3c3bb3
Łukasz Klekner <lukaszklekner@gmail.com>
2021-10-20 18:22:00 +0200

    adds button and handleClick fn

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+  const handleClick = (name, e) => {
+    console.log(`Hello ${name}`, e.target)
+  }
   return (
     <div className='home'>
       <h2>Homepage</h2>
+      <button onClick={(e) => handleClick('Lukas', e)}>Click me!</button>
     </div>
   )
 }
