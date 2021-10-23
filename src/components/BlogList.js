bd8084ed83123fb09c29a0b1c8d0875da0fb7f82
Łukasz Klekner <lukaszklekner@gmail.com>
2021-10-23 20:46:33 +0200

    adds Error state

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,7 +1,7 @@
 const BlogList = ({ blogs, title }) => (
   <div className='blog-list'>
     <h2>{title}</h2>
-    {blogs.map(({ id, title, author }) => (
+    {blogs?.map(({ id, title, author }) => (
       <div className='blog-preview' key={id}>
         <h2>{title}</h2>
         <p>Written by: {author}</p>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,18 +4,30 @@ import BlogList from '../components/BlogList'
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
   const [isPending, setIsPending] = useState(true)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
-    fetch(`http://localhost:8000/blogs`)
-      .then((response) => response.json())
+    fetch(`http://localhost:8000/blogss`)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error('Could not fetch data from server')
+        }
+        response.json()
+      })
       .then((data) => {
         setBlogs(data)
+        setIsPending(false)
+        setError(null)
+      })
+      .catch((error) => {
+        setError(error.message)
         setIsPending(false)
       })
   }, [])
 
   return (
     <div className='home'>
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title='All posts' />}
     </div>
