4ce02ef88b87721ddfa557dc4809ddcf537eec2e
Łukasz Klekner <lukaszklekner@gmail.com>
2021-10-25 19:37:11 +0200

    implements BlogDetail component and uses Route params

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Create from './pages/Create'
+import BlogDetails from './components/BlogDetails'
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
             </Route>
             <Route path='/create'>
               <Create />
+            </Route>
+            <Route path='/blogs/:id'>
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
