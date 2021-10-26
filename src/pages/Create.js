478a161ba6c8bb5cf6ef5514c3d2492c6f6eff55
Łukasz Klekner <lukaszklekner@gmail.com>
2021-10-26 14:15:38 +0200

    adds post request to submit fn

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,15 +1,26 @@
 import { useState } from 'react'
 
+const BASE_URL = `http://localhost:8000/blogs`
+
 const Create = () => {
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
   const [author, setAuthor] = useState('mario')
+  const [isPending, setIsPending] = useState(false)
 
   const handleSubmit = (event) => {
     event.preventDefault()
+
+    setIsPending(true)
     const newBlog = { title, body, author }
 
-    console.log(newBlog)
+    fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newBlog),
+    }).then(() => setIsPending(false))
   }
 
   return (
@@ -34,7 +45,8 @@ const Create = () => {
           <option value='mario'>Mario</option>
           <option value='luigi'>Luigi</option>
         </select>
-        <button>Add button</button>
+        {!isPending && <button>Add a blog</button>}
+        {isPending && <button disabled>Adding a blog</button>}
       </form>
     </div>
   )
