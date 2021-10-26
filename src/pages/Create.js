d058b5740e8173eeb15d38520b0f3ccf9f9f3641
Łukasz Klekner <lukaszklekner@gmail.com>
2021-10-26 11:56:39 +0200

    adds states to Create component and adds submit fn

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,41 @@
+import { useState } from 'react'
+
 const Create = () => {
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const [author, setAuthor] = useState('mario')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const newBlog = { title, body, author }
+
+    console.log(newBlog)
+  }
+
   return (
     <div className='create'>
       <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title</label>
+        <input
+          type='text'
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label htmlFor=''>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value='mario'>Mario</option>
+          <option value='luigi'>Luigi</option>
+        </select>
+        <button>Add button</button>
+      </form>
     </div>
   )
 }
